49a6635093370d728339a2386eb4da5d976c1ec3
ddWayontec <demisangdan@gmail.com>
2019-03-10 21:31:43 -0600

    chat app first upload

---
+++ ChitChat/index.js
@@ -0,0 +1,77 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+let express = require('express');
+app.use(express.static(__dirname + '/'));
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
+var usersOnline = 0;
+var usersArr = [];
+var messageArr = [];
+
+io.on('connection', function(socket){
+    usersOnline++;
+    io.emit('update users', usersOnline);
+
+    var isUnique = true;
+    var uniqueName;
+    do {
+        isUnique = true;
+        uniqueName = 'Anonymous' + Math.floor(Math.random() * 500);
+        for(var i=0; i<usersArr.length; i++) {
+            if(uniqueName === usersArr[i]) {
+                isUnique = false;
+            }
+        }
+    } while(isUnique === false)
+    socket.nickname = uniqueName;
+    socket.emit('new user', uniqueName);
+
+    usersArr.push(uniqueName);
+    io.emit('update userlist', usersArr);
+
+    socket.emit('update messagelist', messageArr);
+
+    socket.on('chat message', function(data){
+        if(data.msg.includes('/nickcolor')) {
+            // var mssg = data.msg;
+            // mssg = mssg.split(" ")[1]; //unsafe if client message isnt perfect
+            // newNick = '<span style=color:' + mssg + '>' + data.name + '</span>';
+        }
+        else if(data.msg.includes('/nick')) {
+            var mssg = data.msg;
+            mssg = mssg.split(" ")[1]; //unsafe if client message isnt perfect
+            var alreadyExists = false;
+            for(var i=0; i<usersArr.length; i++) {
+                if(mssg === usersArr[i]) {
+                    alreadyExists = true;
+                }
+            }
+            if(alreadyExists === false) {
+                usersArr.splice( usersArr.indexOf(socket.nickname), 1 );
+                socket.nickname = mssg;
+                socket.emit('new user', mssg);
+                usersArr.push(mssg);
+                io.emit('update userlist', usersArr);
+            }
+        }
+        else {
+            var date = new Date();
+            var time = date.toLocaleTimeString();
+            messageArr.push(time + ' ' + data.name + ' - ' + data.msg);
+            socket.broadcast.emit('chat message', time + ' ' + data.name + ' - ' + data.msg);
+            socket.emit('chat message', "<b>" + time + ' ' + data.name + ' - ' + data.msg + "</b>")
+        }
+    });
+
+    socket.on('disconnect', function(){
+        usersOnline--;
+        io.emit('update users', usersOnline);
+        usersArr.splice( usersArr.indexOf(socket.nickname), 1 );
+        io.emit('update userlist', usersArr);
+    });
+});
